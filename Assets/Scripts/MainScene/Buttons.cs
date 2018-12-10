b892498aaaaee574e32e9df1671ff9022f9a21ee
Skindrila <skindrila_tv@mail.ru>
2018-12-10 20:41:25 +0300

    add

---
+++ Assets/Scripts/MainScene/Buttons.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour 
+{
+	#region Fields
+	[SerializeField]
+	private Sprite mus_on, mus_off;
+
+	private GameObject screen;
+	private  AudioSource audioClickButton;
+	#endregion
+
+	#region Unity lifecycle
+	void Start()
+	{
+		audioClickButton = GameObject.Find ("clickButton").GetComponent<AudioSource> ();
+		if (gameObject.name == "Music") 
+		{
+			if(PlayerPrefs.GetString ("Music") == "Off")
+			{
+				GetComponent<Image> ().sprite = mus_off;
+				Camera.main.GetComponent<AudioListener> ().enabled = false;
+			}
+		}
+	}
+
+	void OnMouseDown ()
+	{
+		transform.localScale = new Vector3 (1.1f, 1.1f, 1.1f);
+	}
+
+	void OnMouseUp ()
+	{
+		transform.localScale = new Vector3 (1f, 1f, 1f);
+	}
+
+	void OnMouseUpAsButton()
+	{
+		audioClickButton.Play ();
+		switch (gameObject.name) 
+		{
+		case "Home":
+			SceneManager.LoadScene ("main");
+			break;
+		case "Restart":
+			screen = GameObject.FindGameObjectWithTag ("DetectClicks");
+			screen.GetComponent<Main> ().Restart ();
+			break;
+		case "Music":
+			if (PlayerPrefs.GetString ("Music") == "Off")
+			{
+				GetComponent<Image> ().sprite = mus_on;
+				PlayerPrefs.SetString ("Music", "On");
+				Camera.main.GetComponent<AudioListener> ().enabled = true;
+			} 
+			else
+			{
+				Camera.main.GetComponent<AudioListener> ().enabled = false;
+				GetComponent<Image> ().sprite = mus_off;
+				PlayerPrefs.SetString ("Music", "Off");
+			}
+			break;
+		}
+	}
+	#endregion
+}
---
+++ Assets/Scripts/MainScene/GameArrangement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameArrangement : MonoBehaviour 
+{
+	#region Fields
+	[SerializeField]
+	private Text playText;
+	[SerializeField]
+	private GameObject buttons;
+	[SerializeField]
+	private GameObject allObjects;
+
+	private bool isStartPlay;
+	#endregion
+
+	#region Unity lifecycle
+	void start()
+	{
+		isStartPlay = false;
+	}
+
+	void OnMouseUp()
+	{
+		if (!isStartPlay)
+		{
+			isStartPlay = true;
+			playText.gameObject.SetActive (false);
+			buttons.gameObject.SetActive (false);
+			allObjects.GetComponent<Animator> ().Play ("StartGame");
+			StartCoroutine (WaitAnimation());
+			this.GetComponent<Main> ().enabled = true;
+		}
+	}
+	#endregion
+
+	#region Private methods
+	IEnumerator WaitAnimation()
+	{
+		yield return new WaitForSeconds(1.5f);
+		allObjects.GetComponent<Animator> ().enabled = false;
+		allObjects.transform.position = new Vector3 (-2.6f,1.25f,0f);
+	}
+	#endregion
+}
